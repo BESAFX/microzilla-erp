86e60b259c25297c2b71291b82c3ce0a26e90d92
bassamalmahdy <bassamalmahdy@ekfc.ae>
2023-08-15 13:38:59 +0400

    ...

--- src/main/java/com/besafx/app/SetupUI.java
+++ src/main/java/com/besafx/app/SetupUI.java
@@ -6,6 +6,8 @@ import org.springframework.core.io.Resource;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -178,25 +180,22 @@ public class SetupUI extends JFrame {
 
         final PopupMenu popup = new PopupMenu();
         Resource path = new ClassPathResource("icon.png");
-        Image img = null;
+        Image img;
         try {
             img = Toolkit.getDefaultToolkit().getImage(path.getURL());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        final TrayIcon trayIcon = new TrayIcon(img, "Application Name", popup);
+        final TrayIcon trayIcon = new TrayIcon(img, "Micro-Academy", popup);
         final SystemTray tray = SystemTray.getSystemTray();
 
-        // Create a pop-up menu components
         MenuItem aboutItem = new MenuItem("About");
+
         CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
         CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
-        Menu displayMenu = new Menu("Display");
-        MenuItem errorItem = new MenuItem("Error");
-        MenuItem warningItem = new MenuItem("Warning");
-        MenuItem infoItem = new MenuItem("Info");
-        MenuItem noneItem = new MenuItem("None");
+
         MenuItem exitItem = new MenuItem("Exit");
+        exitItem.addActionListener(this::onClickShutdown);
 
         //Add components to pop-up menu
         popup.add(aboutItem);
@@ -204,11 +203,6 @@ public class SetupUI extends JFrame {
         popup.add(cb1);
         popup.add(cb2);
         popup.addSeparator();
-        popup.add(displayMenu);
-        displayMenu.add(errorItem);
-        displayMenu.add(warningItem);
-        displayMenu.add(infoItem);
-        displayMenu.add(noneItem);
         popup.add(exitItem);
 
         trayIcon.setPopupMenu(popup);
